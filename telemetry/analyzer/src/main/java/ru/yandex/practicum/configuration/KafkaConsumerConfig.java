12b5736af817ee0a614382166605f215bc8d01c4
Sergeev Aleksandr <frolvrn36@yandex.ru>
2025-12-09 12:34:59 +0300

    Исправления v.6

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/configuration/KafkaConsumerConfig.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/configuration/KafkaConsumerConfig.java
@@ -4,66 +4,53 @@ import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import ru.yandex.practicum.kafka.deserializer.HubEventDeserializer;
+import ru.yandex.practicum.kafka.deserializer.SensorsSnapshotDeserializer;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
 
 import java.util.Properties;
 
-/**
- * Конфигурация Kafka Consumer для приложения анализатора.
- * Создает отдельные консьюмеры для обработки снапшотов и событий от хабов.
- */
 @Configuration
 @RequiredArgsConstructor
 public class KafkaConsumerConfig {
 
     private final Environment environment;
 
-    /**
-     * Создает консьюмер для обработки снапшотов сенсоров.
-     *
-     * @return Consumer для обработки SensorsSnapshotAvro
-     */
     @Bean
     public Consumer<String, SensorsSnapshotAvro> getSnapshotConsumer() {
         Properties config = new Properties();
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                environment.getProperty("spring.kafka.bootstrap-servers"));
+                environment.getProperty("kafka.bootstrap-servers"));
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-                environment.getProperty("spring.kafka.consumer.snapshots.key-deserializer"));
+                StringDeserializer.class.getName());
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                environment.getProperty("spring.kafka.consumer.snapshots.value-deserializer"));
-        config.put(ConsumerConfig.CLIENT_ID_CONFIG,
-                environment.getProperty("spring.kafka.consumer.snapshots.client-id"));
+                SensorsSnapshotDeserializer.class.getName());
+        config.put(ConsumerConfig.CLIENT_ID_CONFIG, "analyzer-snapshot-consumer");
         config.put(ConsumerConfig.GROUP_ID_CONFIG,
-                environment.getProperty("spring.kafka.consumer.snapshots.group-id"));
+                environment.getProperty("kafka.group-id.snapshot"));
         config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
         return new KafkaConsumer<>(config);
     }
 
-    /**
-     * Создает консьюмер для обработки событий от хабов.
-     *
-     * @return Consumer для обработки HubEventAvro
-     */
     @Bean
     public Consumer<String, HubEventAvro> getHubEventConsumer() {
         Properties config = new Properties();
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                environment.getProperty("spring.kafka.bootstrap-servers"));
+                environment.getProperty("kafka.bootstrap-servers"));
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-                environment.getProperty("spring.kafka.consumer.hub.key-deserializer"));
+                StringDeserializer.class.getName());
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                environment.getProperty("spring.kafka.consumer.hub.value-deserializer"));
-        config.put(ConsumerConfig.CLIENT_ID_CONFIG,
-                environment.getProperty("spring.kafka.consumer.hub.client-id"));
+                HubEventDeserializer.class.getName());
+        config.put(ConsumerConfig.CLIENT_ID_CONFIG, "analyzer-hub-consumer");
         config.put(ConsumerConfig.GROUP_ID_CONFIG,
-                environment.getProperty("spring.kafka.consumer.hub.group-id"));
+                environment.getProperty("kafka.group-id.hub"));
         config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
